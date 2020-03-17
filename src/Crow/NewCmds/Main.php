c89301be9526a525f2532e24df0dc49652872ac1
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-03-17 13:38:30 +0100

    Update Main.php

--- src/Crow/NewCmds/Main.php
+++ src/Crow/NewCmds/Main.php
@@ -1,41 +1,39 @@
 <?php
 
-namespace Crow
+namespace Crow;
 
-use pocketmine\command\Command;
-
-use pocketmine\command\CommandSender;
-
+use pocketmine\Server;
+use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-
 use pocketmine\utils\TextFormat as A;
 
-class Main extends PluginBase
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
-    {
+class Main extends PluginBase{
 
-    public function onEnable()
+    public function onEnable(){
+    }
 
-    {
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
 
-      $this->getLogger()->info("Aktiviert")
+        switch($cmd->getName()){
+
+            case "Heal":
+                if($sender instanceof Player){
+			if($sender->hasPermission("Heal.cmd")){
+				$sender->setHealt(20);
+				$sender->sendMessage(A::YELLOW . "Du Hast" . A::AQUA . "$sender->GetName()" . A::YELLOW . "Geheilt");
+			}
+			   
+                }
+            break;
+
+        }
+
+        return true;
 
     }
 
-    public function onCommand(CommandSender $sender,Command $cmd,string $label,array $args) :bool
-
-    {
-
-      switch($cmd->GetName()){
-
-        case "Heal":
-
-            $sender->sendMessage(A::YELLOW . "Du Hast" . A::AQUA . "$sender->GetName()" . A::YELLOW . "Geheilt");
-
-            $sender->setHealt(20);
-
-         }
-
-      }
-
-  }
+}
+		
