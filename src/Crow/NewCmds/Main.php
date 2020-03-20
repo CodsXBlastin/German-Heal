18b7304179afeb93c796114df1aa340ebe24cf9a
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-03-20 21:35:04 +0100

    Main.php
    mhh vlt. fixed

--- src/Crow/NewCmds/Main.php
+++ src/Crow/NewCmds/Main.php
@@ -5,8 +5,6 @@ namespace Crow;
 use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat as A;
-
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
@@ -18,12 +16,11 @@ class Main extends PluginBase{
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
 
         switch($cmd->getName()){
-
             case "Heal":
                 if($sender instanceof Player){
 			if($sender->hasPermission("Heal.cmd")){
 				$sender->setHealt(20);
-				$sender->sendMessage(A::YELLOW . "Du Hast" . A::AQUA . "$sender->GetName()" . A::YELLOW . "Geheilt");
+				$sender->sendMessage("Du Hast $sender->GetName() Geheilt");
 			}
 			   
                 }
