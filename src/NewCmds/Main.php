90d69ddda241a1f08f8bc8920345ae894f996a90
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-03-20 21:37:20 +0100

    Create Main.php

---
+++ src/NewCmds/Main.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Crow;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase{
+
+    public function onEnable(){
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
+
+        switch($cmd->getName()){
+            case "Heal":
+                if($sender instanceof Player){
+			if($sender->hasPermission("Heal.cmd")){
+				$sender->setHealt(20);
+				$sender->sendMessage("Du Hast $sender->GetName() Geheilt");
+			}
+			   
+                }
+            break;
+
+        }
+
+        return true;
+
+    }
+
+}
