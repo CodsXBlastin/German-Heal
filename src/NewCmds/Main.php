07409a310277ff0a8610c02deb27b58c98afbea2
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-03-20 21:47:46 +0100

    Update Main.php

--- src/NewCmds/Main.php
+++ src/NewCmds/Main.php
@@ -11,6 +11,15 @@ use pocketmine\command\CommandSender;
 class Main extends PluginBase{
 
     public function onEnable(){
+    
+    $this->getLogger()->info("Plugin aktiviert");
+
+    }
+    
+    public function onDisable(){
+
+    $this->getLogger()->info("Plugin Deaktiviert");
+
     }
 
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
